7054147e82eb8ceae4c10478c162f4d87b01926d
Pratham Choksi <69908727+prathch2@users.noreply.github.com>
2024-03-26 23:36:32 -0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Demo',
+    version='0.1',
+    description='package practice for m09 classwork',
+    url='https://github.com/prathch2/ParticipationM09/Demo',
+    author='Pratham Choksi',
+    author_email='prathch2@gmail.com',
+    license='Apache License 2.0',
+    packages=['demo']
+)
